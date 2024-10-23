c554489d5cf909836243243eb8c3cafcd772dc21
BaseerAhmed6007 <148212098+BaseerAhmed6007@users.noreply.github.com>
2024-10-23 10:50:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -312,8 +312,8 @@ def analyze_layout(file_path):
 
     # Aggregate the results and return
     #After processing, you can handle the aggregated text
-    full_text = " ".join(aggregated_text)
-    return full_text
+    #full_text = " ".join(aggregated_text)
+    #return full_text
 
 def analyze_document_app():
     response = None  # Initialize response
