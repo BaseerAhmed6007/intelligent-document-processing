a236420a21ac2bdfb16a090866d005af7795272a
BaseerAhmed6007 <148212098+BaseerAhmed6007@users.noreply.github.com>
2024-10-23 10:05:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def analyze_document_app():
             if user_command:
                 intent = recognize_intent(user_command)
                 response_message = process_intent(intent, result_text)
-            st.text_area("Output", value=response_message, height=400)
+                st.text_area("Output", value=response_message, height=400)
                 #st.write(f"Command Response: {response_message}")
                 #st.text_area("Output", value=response_message, height=300)
 
