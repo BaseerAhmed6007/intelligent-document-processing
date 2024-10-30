f2e64d9c4a8321b67de9196d45cf77683f13987e
BaseerAhmed6007 <148212098+BaseerAhmed6007@users.noreply.github.com>
2024-10-30 15:26:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from docx import Document
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 from azure.ai.language.conversations import ConversationAnalysisClient
-from openai import AzureOpenAI
+#from openai import AzureOpenAI
 import numpy as np
 import openai
 import re
